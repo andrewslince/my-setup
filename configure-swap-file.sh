0fde5fbcc3d67c21354de10391ec7acb37777dee
Andrews Lince <andrews.lince@gmail.com>
2017-03-19 07:18:06 -0400

    Added script to configure swap file

---
+++ configure-swap-file.sh
@@ -0,0 +1,14 @@
+#
+# https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-16-04
+#
+
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo vm.swappiness=10 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+sudo sysctl vm.vfs_cache_pressure=50
+echo vm.vfs_cache_pressure=50 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
