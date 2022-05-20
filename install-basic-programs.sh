1585316fcb04c2557cce58adde220abb5debd981
Andrews Lince <andrews.lince@gmail.com>
2022-05-20 09:32:02 -0300

    fix: set python3 as default

--- install-basic-programs.sh
+++ install-basic-programs.sh
@@ -14,4 +14,7 @@ sudo apt install -y \
     inkscape \
     gnome-tweaks
 
+# set "python3" as default python version (credits: https://stackoverflow.com/a/50331137)
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+
 exit 0
