ba3451dcbee419c0b70cb5e1112de9380b89f666
Andrews Lince <andrews.lince@gmail.com>
2024-10-01 08:52:59 -0300

    feat(install-git): add feature to show current git branch in terminal

--- install-git.sh
+++ install-git.sh
@@ -41,3 +41,12 @@ git config --global color.diff.new "green"
 git config --global color.status.added "yellow"
 git config --global color.status.changed "green"
 git config --global color.status.untracked "cyan"
+
+# show the current git branch in terminal
+cat >> ~/.bashrc <<EOL
+# show the current git branch in terminal
+PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;33m\]\$(__git_ps1 " [%s]")\[\033[0m\] \[\033[00m\]\$ '
+
+EOL
+
+source ~/.bashrc
