9fb1f0c209c1e470764392b3e315297e166782d7
Andrews Lince <andrews.lince@gmail.com>
2025-06-05 08:19:13 -0300

    chore(install-nvm): upgrade nvm version

--- install-nvm.sh
+++ install-nvm.sh
@@ -5,6 +5,6 @@ set -e
 # install update packapegs
 sudo apt update -y
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 nvm install --lts
