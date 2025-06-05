60225a113cf0ed08a9844015f2b08db0b8520e93
Andrews Lince <andrews.lince@gmail.com>
2025-06-05 08:21:15 -0300

    feat(main installer): reorder package installations

--- install.sh
+++ install.sh
@@ -12,13 +12,15 @@ source ./install-nvm.sh
 
 source ./install-vscode.sh
 
-source ./install-1password-cli.sh
-
-source ./install-1password-cli.sh
-
-source ./install-github-cli.sh
+#source ./install-github-cli.sh
 
 source ./configure-basic-bash-aliases.sh
+
+source ./install-1password.sh
+
+#source ./install-1password-cli.sh
+
+source ./install-python-tools.sh
 
 # install docker (https://docs.docker.com/engine/install/ubuntu/)
 
