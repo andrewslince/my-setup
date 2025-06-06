35f9b4e9933c0c950a316d6d7e393b4549e34ad7
Andrews Lince <andrews.lince@gmail.com>
2025-06-06 09:50:30 -0300

    chore(main-installer): remove unnecessary code

--- install.sh
+++ install.sh
@@ -12,18 +12,8 @@ source ./install-nvm.sh
 
 source ./install-vscode.sh
 
-#source ./install-github-cli.sh
-
 source ./configure-basic-bash-aliases.sh
 
 source ./install-1password.sh
 
-#source ./install-1password-cli.sh
-
 source ./install-python-tools.sh
-
-# install docker (https://docs.docker.com/engine/install/ubuntu/)
-
-# install docker compose (https://docs.docker.com/compose/install/)
-
-# install vscode extensions
