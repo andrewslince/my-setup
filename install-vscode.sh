12eddc38e5a03f863c1bb9336c6ebf61d57f2858
Andrews Lince <andrews.lince@gmail.com>
2025-07-30 12:45:20 -0300

    chore(install-vs-code): add confirm param to add repository

--- install-vscode.sh
+++ install-vscode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 sudo apt update -y
 
@@ -8,7 +8,7 @@ sudo apt install software-properties-common apt-transport-https wget -y
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
 
 sudo apt install code -y
 
