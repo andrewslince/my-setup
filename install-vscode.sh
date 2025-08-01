33af3d2d3d3de43030a88e788fdac0c79a55a125
Andrews Lince <andrews.lince@gmail.com>
2025-08-01 05:40:29 -0300

    fix(install-vscode): install vscode the right way

--- install-vscode.sh
+++ install-vscode.sh
@@ -2,15 +2,14 @@
 
 #set -e
 
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+rm -Rf packages.microsoft.gpg
+
 sudo apt update -y
 
-sudo apt install software-properties-common apt-transport-https wget -y
-
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
-
 sudo apt install code -y
-
-code --version
-
