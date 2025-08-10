01b6f68d07ac570234a144705e4320420f1b9ca8
Andrews Lince <andrews.lince@gmail.com>
2025-08-10 07:20:40 -0300

    feat(docker-install-compose): detect latest version
    implemented automatically detection from compose latest version.

--- docker/docker-install-compose.sh
+++ docker/docker-install-compose.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
-set -e
+# Define o local de instalação
+INSTALL_DIR="/usr/local/bin"
+DOCKER_COMPOSE_BIN="$INSTALL_DIR/docker-compose"
 
-# CREDITS: https://docs.docker.com/compose/install/
+echo "Buscando o último release do Docker Compose..."
 
-clear
+# Pega o URL do último release usando a API do GitHub
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[] | select(.name | contains("docker-compose-'"$OS"'-'"$ARCH"'")) | .browser_download_url')
 
-VERSION="1.25.5"
+if [ -z "$LATEST_RELEASE_URL" ]; then
+    echo "Erro: Não foi possível encontrar o URL de download para a arquitetura do seu sistema."
+    exit 1
+fi
 
-# downloading docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+LATEST_RELEASE_URL_STR=($LATEST_RELEASE_URL)
+LATEST_RELEASE_URL=${LATEST_RELEASE_URL_STR[0]}
 
-# setting permissions to execute docker compose
-sudo chmod +x /usr/local/bin/docker-compose
+echo "Baixando o Docker Compose da URL '$LATEST_RELEASE_URL'..."
 
-# print version from docker compose to check installation
-docker-compose --version
+# Baixa o binário
+sudo curl -L "$LATEST_RELEASE_URL" -o "$DOCKER_COMPOSE_BIN"
+
+if [ $? -ne 0 ]; then
+    echo "Erro: Falha ao baixar o Docker Compose."
+    exit 1
+fi
+
+echo "Definindo permissões de execução..."
+
+# Torna o binário executável
+sudo chmod +x "$DOCKER_COMPOSE_BIN"
+
+if [ $? -ne 0 ]; then
+    echo "Erro: Falha ao definir permissões de execução."
+    exit 1
+fi
+
+echo "Docker Compose instalado/atualizado com sucesso!"
+
+# Verifica a versão instalada
+echo "Versão instalada:"
+docker compose version
