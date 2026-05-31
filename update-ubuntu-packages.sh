3c9f6df7f34f196ca42c734405eac8901e718213
Andrews Lince <andrews.lince@gmail.com>
2026-05-31 16:12:24 -0300

    chore(update-ubuntu-packages): modernize packages

--- update-ubuntu-packages.sh
+++ update-ubuntu-packages.sh
@@ -4,17 +4,23 @@
 # Credits: http://askubuntu.com/a/196777
 #
 
+sudo apt modernize-sources -y
+
 # fetches the list of available updates
 sudo apt update -y
 
 # strictly upgrades the current packages
 sudo apt upgrade -y
 
+sudo apt full-upgrade -y
+
 # installs updates (new ones)
 sudo apt dist-upgrade -y
 
 # remove unused linux headers and old packages
 sudo apt autoremove -y
+
+sudo apt autoclean -y
 
 # update npm version
 npm install -g npm
